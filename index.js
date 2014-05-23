05aac5bd48b8aea271a5568376371845debbeb71
sergey <sergey@debianServer>
2014-05-23 10:37:22 +0300

    added promise

--- index.js
+++ index.js
@@ -1,70 +1,62 @@
 module.exports = function(io, dbConfig) {
 	var rules  = {},
 		getters = {},
-		mongoClient = require('mongodb').MongoClient,	
-		getCollection = (function(){
-			var collection;
-			return function (callback) {
-				if(collection) { 
-					callback(collection) 
-				} else {
-					mongoClient.connect('mongodb://'+dbConfig.host+':'+(dbConfig.post || '27017')+'/'+(dbConfig.db || "sockets"), function(err, res){
-						collection = res.collection(dbConfig.collection || "sockets");
-						callback(collection);
-					});
-				}
-			}
+		mongoClient = require('mongodb').MongoClient,
+		q = require("promise"),
+		connect = q.denodeify(function(callback){
+			mongoClient.connect('mongodb://'+dbConfig.host+':'+(dbConfig.post || '27017')+'/'+(dbConfig.db || "sockets"), function(err, result){
+				callback(err, result && result.collection && result.collection(dbConfig.collection || "sockets"))
+			});			
 		})(),
 		dsInstance = function(socket, _id){
-			this.socketID = socket.id
+			if(!socket) {throw new Error("socket parameter is not defined")};
+			if(!_id) {throw new Error("_id parameter is not defined")};
+			this.socketID = socket.id;
 			this._id = _id || socket.id;
 			var _self = this;
+			connect.then(function(collection) {
+				_self.collection = collection;
+				_self.itemCreatePromise = q.denodeify(_self.collection.update).call(_self.collection, {_id : _self._id}, {$set : {socketID : _self.socketID}}, {upsert : true});
+			})
 			socket.on("disconnect", function(){
-				getCollection(function(collection){
-					collection.remove({socketID : _self.socketID}, function(err){
-						!err && (delete _self);
-					});					
-				});
+				//getCollection(function(collection){
+					delete _self;			
+				//});
 			});
 		};
 		
 	dsInstance.prototype = {
 		set : function(data, callback){
 			var _self = this;
-			getCollection(function(collection){
-				data["$set"] = data["$set"] || {}
-				data["$set"].socketID = _self.socketID;
-				collection.update({_id : _self._id}, data, {upsert : true}, callback || (function(err, result){
-					console.log(arguments)
-				}))
-			});			
+			this.itemCreatePromise.then(function(res){
+				_self.collection.update({_id : _self._id}, data, callback);
+			});
 		},
 		send : function(event, params, data, callback) {
 			var eventRules = rules[event] || {},
 				_self = this;
-				
-			getCollection(function(collection){
-				collection.findOne({socketID : _self.socketID}, function(err, currentSocketData){					
-					var currentSocketData = currentSocketData || {},
-						query  = JSON.parse(eventRules.replace(/\"?__(me|param)\.(\w+)\"?/g, function(str, source, prop){
+			this.itemCreatePromise.then(function(err, res){
+				_self.collection.findOne({_id : _self._id}, function(err, currentSocketData){
+					console.log("------------------------------");
+					console.log(currentSocketData);
+					var currentSocketData = currentSocketData || {};
+					var query  = JSON.parse(eventRules.replace(/\"?__(me|param)\.(\w+)\"?/g, function(str, source, prop){
 							var sourceObj = source == "me" ? currentSocketData : params;
-							console.log(sourceObj);
 							if(sourceObj[prop]) {
-								return sourceObj[prop];
+								return JSON.stringify(sourceObj[prop]);
 							}
 							else {
 								throw new Error(str + " is not found");
 							}
 						}).replace(/\"?__(\w+)\((.+?)\)\"?/g, function(str, getter, args){
 							if(typeof getters[getter] == "function") {
-								var result = getters[getter].apply(getters, (args && args.split(",")))
-								return typeof result == "string" ? '"'+reuslt+'"' : result;
+								return JSON.stringify(getters[getter].apply(getters, (args && args.split(","))));
 							} else {
 								throw new Error(getter+" getter is not function or not defined");
 							}							
 						}));					
 					query.socketID = {$ne : _self.socketID};
-					collection.find(query, function(err, result){						
+					_self.collection.find(query, function(err, result){						
 						result.each(function(err, record){		
 							record && io.sockets.socket(record.socketID).emit(event, data);
 						})
@@ -79,11 +71,12 @@ module.exports = function(io, dbConfig) {
 				rules[key] = JSON.stringify(dataRules[key]);
 			}
 		},
-		getInstane : function(socket, _id){
+		getInstance : function(socket, _id){
 			return new dsInstance(socket, _id);
 		},
 		setupGetter : function(name, getter) {
 			getters[name] = getter;
+			return this;
 		}
 	}
 }
